af9dbde52cf81290b93a861c048dc8f44c5282c4
mgltorsa <mgltorsa@gmail.com>
2020-03-30 02:40:38 -0500

    add api resources

--- app/src/main/java/com/example/cloudapp/model/AWSS3Connection.java
+++ app/src/main/java/com/example/cloudapp/model/AWSS3Connection.java
@@ -1,4 +1,12 @@
 package com.example.cloudapp.model;
 
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+
 public class AWSS3Connection {
+
+    private AmazonS3Client client;
+    private BasicAWSCredentials credentials;
+
+    
 }
